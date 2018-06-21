e2d4b39c8a52b6d29a5adc4ba38189586e868c3f
sssssssl <sl.sl.ssss.sssl@gmail.com>
2018-06-21 19:29:36 +0800

    notification plus

---
+++ ding.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name         North-Plus Notification Plus
+// @namespace    https://github.com/sssssssl/NP-scripts
+// @version      0.1
+// @description  发帖时自动勾选新回复通知按钮，增强通知显示效果
+// @author       sl
+// @match        https://*.white-plus.net/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const url = window.location.href;
+    const THREAD_PAT = /read\.php\?tid/;
+    const POST_PAT = /post\.php\?fid-9/;
+    const REPLY_PAT = /post\.php\?action-reply/;
+
+    const ID_NEW_RP = 'newrp';
+    const CLS_BLINK = 'blink';
+    const NEW_RP_STYLE = `
+        .${CLS_BLINK} {
+            display: inline-block; font-weight:bold;
+            color:#FF0000;
+            animation:blink 1s;
+            animation-iteration-count:infinite;
+            -webkit-animation: blink 1s;
+            -webkit-animation-iteration-count: infinite;
+        }
+        @keyframes blink {
+            0% { transform: rotate(0deg) scale(1, 1); }
+            25% { transform: rotate(15deg) scale(1.2, 1.2); }
+            50% { transform: rotate(-15deg) scale(1.2, 1.2); }
+            100% { transform: rotate(0deg) scale(1, 1); }
+        }
+        @-webkit-keyframes blink {
+            0% { transform: rotate(0deg) scale(1, 1); }
+            25% { transform: rotate(15deg) scale(1.2, 1.2); }
+            50% { transform: rotate(-15deg) scale(1.2, 1.2); }
+            100% { transform: rotate(0deg) scale(1, 1); }
+        }
+    `;
+
+    let tagNewRP = document.getElementById(ID_NEW_RP);
+    if(tagNewRP) {
+        let styleTag = document.createElement('style');
+        styleTag.textContent = NEW_RP_STYLE;
+        document.head.appendChild(styleTag);
+        tagNewRP.classList.add(CLS_BLINK);
+        tagNewRP.addEventListener('click', (e) => {
+            tagNewRP.classList.remove(CLS_BLINK);
+        });
+    }
+
+    if(POST_PAT.test(url) || REPLY_PAT.test(url)) {
+        const postOptions = document.getElementById('post-option');
+        if(postOptions) {
+            const box = document.querySelector('input[name=atc_newrp]');
+            box.setAttribute('checked', 'checked');
+        }
+    }
+})();
